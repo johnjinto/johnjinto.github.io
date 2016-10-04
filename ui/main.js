cc060ef191dd910f3d06f9a402493dacaf3052fa
johnjinto <johnjinto@users.noreply.github.com>
2016-10-04 08:01:01 +0530

    added motion controls

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,6 @@
-console.log('Loaded!');
-
-//var element = document.getElementById('phrase');
-
-//element.innerHTML = 'Jesus is God';
-
+/* move angel to right
 var img = document.getElementById('pup');
+
 
 var marginLeft = 0;
 function moveRight(){ 
@@ -19,3 +15,36 @@ img.onclick = function (){
 var interval = setInterval(moveRight, 100); 
 
 };
+*/
+var img = document.getElementById('angel');
+
+var flydwn = document.getElementById('downbutton');
+var flyup = document.getElementById('upbutton');
+var flyryt = document.getElementById('rightbutton');
+var flyleft = document.getElementById('leftbutton');
+
+
+var topinc = parseInt(img.style.top, 10);
+var leftinc = parseInt(img.style.left, 10);
+
+flydwn.onclick = function(){	
+topinc = topinc+5;
+img.style.top = topinc + 'px';
+}
+
+flyup.onclick = function(){	
+topinc = topinc-5;
+img.style.top = topinc + 'px';
+}
+
+flyryt.onclick = function(){	
+leftinc = leftinc+5;
+img.style.left = leftinc + 'px';
+
+}
+
+flyleft.onclick = function(){	
+leftinc = leftinc-5;
+img.style.left = leftinc + 'px';
+
+}
